de4602790f17d2182ac30cd628e30baf78578568
Jaapjan Tinbergen <jaapjan@domain.local>
2017-04-18 21:13:31 +0200

    Configurable mail transport

--- src/plugins/mail.js
+++ src/plugins/mail.js
@@ -1,15 +1,7 @@
-const nodemailer = require('nodemailer');
-
 let transporter = null;
 
-const configureTransporter = () => {
-    transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'gmail.user@gmail.com',
-            pass: 'yourpass',
-        },
-    });
+const configureMailTransport = (mailTransporter) => {
+    transporter = mailTransporter;
 };
 
 const createBinaryAttachment = (filename, data) => ({
@@ -19,34 +11,14 @@ const createBinaryAttachment = (filename, data) => ({
 
 const sendMail = async (to, from, subject, msgText, cc, bcc, attachments) => {
     if (!transporter) {
-        throw new Error('No email transport defined yet. Unable to send mail.');
+        return Promise.reject('No email transport defined yet. Unable to send mail.');
     }
 
-    const mailOptions = {
-        from: from.split(',')[0],
-        to,
-        subject,
-        cc,
-        bcc,
-        text: msgText.indexOf('html') === -1 ? msgText : null,
-        html: msgText.indexOf('html') !== -1 ? msgText : null,
-        attachments,
-    };
-
-    return new Promise((resolve, reject) => {
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                reject(error);
-                return;
-            }
-
-            resolve(info);
-        });
-    });
+    return transporter(to, from, subject, msgText, cc, bcc, attachments);
 };
 
 module.exports = {
-    configureTransporter,
+    configureMailTransport,
     createBinaryAttachment,
     sendMail,
 };
