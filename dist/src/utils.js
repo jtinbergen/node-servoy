82ab5b2f7a47a6d5177260eb3ff19ea419301212
Dennis <dennis@minox.nl>
2023-09-14 08:56:14 +0200

    Upgrade to 1.5.0

--- dist/src/utils.js
+++ dist/src/utils.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.stringTrim = exports.stringRight = exports.stringMiddle = exports.stringLeft = exports.stringInitCap = exports.stringIndexReplace = exports.numberFormat = exports.getUnicodeCharacter = void 0;
+exports.stringReplace = exports.stringPosition = exports.stringPatternCount = exports.stringToNumber = exports.stringRightWords = exports.stringMiddleWords = exports.stringLeftWords = exports.stringWordCount = exports.stringTrim = exports.stringRight = exports.stringMiddle = exports.stringLeft = exports.stringInitCap = exports.stringIndexReplace = exports.numberFormat = exports.getUnicodeCharacter = void 0;
 const getUnicodeCharacter = (unicodeCharacterNumber) => {
     return String.fromCharCode(unicodeCharacterNumber);
 };
@@ -35,4 +35,45 @@ const stringTrim = (textString) => {
     return (textString || '').trim();
 };
 exports.stringTrim = stringTrim;
+const stringWordCount = (text) => {
+    return (text || '').split(' ').length;
+};
+exports.stringWordCount = stringWordCount;
+const stringLeftWords = (text, numberOfWords) => {
+    return text.split(' ').slice(0, numberOfWords).join(' ');
+};
+exports.stringLeftWords = stringLeftWords;
+const stringMiddleWords = (text, start, numberOfWords) => {
+    return text
+        .split(' ')
+        .slice(start - 1, start - 1 + numberOfWords)
+        .join(' ');
+};
+exports.stringMiddleWords = stringMiddleWords;
+const stringRightWords = (text, numberOfWords) => {
+    return text.split(' ').slice(-numberOfWords).join(' ');
+};
+exports.stringRightWords = stringRightWords;
+const stringToNumber = (textString, decimalSeparator) => {
+    return parseFloat((textString || '').replace(/[^0-9.]/g, '').replace('.', decimalSeparator));
+};
+exports.stringToNumber = stringToNumber;
+const stringPatternCount = (text, toSearchFor) => {
+    return (text.match(new RegExp(toSearchFor, 'g')) || []).length;
+};
+exports.stringPatternCount = stringPatternCount;
+const stringPosition = (textString, toSearchFor, start, occurrence) => {
+    const positions = [];
+    let pos = textString.indexOf(toSearchFor, start - 1);
+    while (pos !== -1) {
+        positions.push(pos);
+        pos = textString.indexOf(toSearchFor, pos + 1);
+    }
+    return positions[occurrence - 1] + 1 || -1;
+};
+exports.stringPosition = stringPosition;
+const stringReplace = (text, searchText, replacementText) => {
+    return text.replace(new RegExp(searchText, 'g'), replacementText);
+};
+exports.stringReplace = stringReplace;
 //# sourceMappingURL=utils.js.map
--- dist/src/utils.test.js
+++ dist/src/utils.test.js
@@ -17,14 +17,38 @@ describe('utils', () => {
     test('stringLeft', async () => {
         expect(utils.stringLeft('this is a test', 6)).toBe('this i');
     });
-    test('stringRight', async () => {
-        expect(utils.stringRight('this is a test', 6)).toBe('a test');
-    });
     test('stringMiddle', async () => {
         expect(utils.stringMiddle('this is a test', 2, 3)).toBe('his');
     });
+    test('stringRight', async () => {
+        expect(utils.stringRight('this is a test', 6)).toBe('a test');
+    });
     test('stringTrim', async () => {
         expect(utils.stringTrim('   text   ')).toBe('text');
+    });
+    test('stringWordCount', async () => {
+        expect(utils.stringWordCount('this is a test')).toBe(4);
+    });
+    test('stringLeftWords', async () => {
+        expect(utils.stringLeftWords('this is a test', 3)).toBe('this is a');
+    });
+    test('stringMiddleWords', async () => {
+        expect(utils.stringMiddleWords('this is a test', 2, 2)).toBe('is a');
+    });
+    test('stringRightWords', async () => {
+        expect(utils.stringRightWords('this is a test', 3)).toBe('is a test');
+    });
+    test('stringToNumber', async () => {
+        expect(utils.stringToNumber('fg65gf.567', '.')).toBe(65.567);
+    });
+    test('stringPatternCount', async () => {
+        expect(utils.stringPatternCount('this is a test', 'is')).toBe(2);
+    });
+    test('stringPosition', async () => {
+        expect(utils.stringPosition('This is a test', 's', 1, 1)).toBe(4);
+    });
+    test('stringReplace', async () => {
+        expect(utils.stringReplace('these are test 1 and test 2.', 'test', 'cow')).toBe('these are cow 1 and cow 2.');
     });
 });
 //# sourceMappingURL=utils.test.js.map
